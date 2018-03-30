cae972471efcb11cbf3071251fde7bf323aaa9b3
baur <baurdotnet@gmail.com>
2018-03-30 22:24:15 +0600

    improve

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -2,18 +2,22 @@ import { cons } from 'hexlet-pairs';
 import getRandomInt from './game-utils';
 import gameAPI from '..';
 
-const gcd = (expression) => {
+const gcd = (n, m) => {
+  if (m === 0) return n;
+  return gcd(m, n % m);
+};
+
+const getAnswer = (expression) => {
   const numArray = expression.split(' ');
   const n = Number(numArray[0]);
   const m = Number(numArray[1]);
-  if (m === 0) return n;
-  return gcd(`${m} ${n % m}`);
+  return gcd(n, m);
 };
 
 const playGameGCD = () => {
   const gameRule = 'Find the greatest common divisor of given numbers.';
   const questionText = () => `${getRandomInt(6, 36)} ${getRandomInt(6, 36)}`;
-  const correctAnswer = () => expression => gcd(expression);
+  const correctAnswer = () => expression => getAnswer(expression);
   const QandA = () => cons(questionText, correctAnswer);
   gameAPI(QandA, gameRule);
 };
